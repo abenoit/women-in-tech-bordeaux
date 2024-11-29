2103d12d19b7bc2079736ce7376850eb1a748fec
Amelie Benoit <amelie.benoit@busbud.com>
2024-11-29 13:52:03 +0100

    init repo

---
+++ tailwind.config.mjs
@@ -0,0 +1,37 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+      mainPurple: "#1E1336",
+      lightPurple: "#7298FB",
+      buttonPurple: "#975EE0",
+      pink: "#FFB6C2",
+      white: "#FFF",
+      black: "#000",
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      borderRadius: {
+        sm: "10px",
+      },
+      spacing: {
+        1: "8px",
+        2: "12px",
+        3: "16px",
+        4: "24px",
+        5: "32px",
+        6: "48px",
+      },
+    },
+  },
+  plugins: [],
+};
